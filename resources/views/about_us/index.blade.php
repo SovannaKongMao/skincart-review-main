567456f647761126bfc7de1db133c3a0a0aa4b00
SovannaKongMao <sovannakongmao@gmail.com>
2024-01-28 22:20:34 +0700

    Main
    Main

---
+++ resources/views/about_us/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.template')
+@section('content')
+<div class="welcome">
+    <div class="container">
+        <div class="welcome-top heading">
+            <h3>ABOUT US</h3>
+            <div class="welcome-bottom">
+                <img src="images/abt-1.jpg" alt="">
+                <p>At SKINCART REVIEW, we're passionate about helping you find the best skincare products for your unique needs. We're a team of skincare experts from all over the world who are dedicated to providing you with honest, unbiased reviews of products from a variety of countries.</p>
+                <p>We know that everyone's skin is different, so we take a personalized approach to our reviews. We consider your skin type, concerns, and budget when recommending products. We also believe in the importance of using high-quality ingredients, so we only recommend products that are made with the best possible ingredients.</p>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- <div class="team">
+    <div class="container">
+        <div class="team-top heading">
+            <h3>OUR TEAM</h3>
+        </div>
+        <div class="team-bottom">
+            <div class="col-md-3 team-left">
+                <img src="images/t-1.jpg" alt="">
+                <h4>Rita Nelson</h4>
+                <p>Fusce at elementum diam. Integer pellentesque ultricies pharetra.</p>
+            </div>
+            <div class="col-md-3 team-left">
+                <img src="images/t-2.jpg" alt="">
+                <h4>Marta Healy</h4>
+                <p>Fusce at elementum diam. Integer pellentesque ultricies pharetra.</p>
+            </div>
+            <div class="col-md-3 team-left">
+                <img src="images/t-3.jpg" alt="">
+                <h4>John Black</h4>
+                <p>Fusce at elementum diam. Integer pellentesque ultricies pharetra.</p>
+            </div>
+            <div class="col-md-3 team-left">
+                <img src="images/t-4.jpg" alt="">
+                <h4>Kate Tompson</h4>
+                <p>Fusce at elementum diam. Integer pellentesque ultricies pharetra.</p>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div> --}}
+@endsection
